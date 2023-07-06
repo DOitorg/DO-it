459e768af4fe22db3e234d38882d934072e4420b
subh-23 <subhodiproy281@gmail.com>
2023-07-06 21:43:48 +0530

    added dummy component

---
+++ components/dummy.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const dummy = () => {
+  return (
+    <View>
+      <Text>dummy</Text>
+    </View>
+  )
+}
+
+export default dummy
+
+const styles = StyleSheet.create({})
