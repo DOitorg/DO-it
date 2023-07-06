fdff7a6c72e87251875e6ab42af2139db8991d73
subh-23 <subhodiproy281@gmail.com>
2023-07-06 23:16:30 +0530

    added singleGroup view routing logic

--- pages/GroupScreen.jsx
+++ pages/GroupScreen.jsx
@@ -1,10 +1,37 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 const GroupScreen = ({ enrollHandler }) => {
   return (
+    <Stack.Navigator initialRouteName="ListOfGroups">
+      <Stack.Screen name="ListOfGroups" component={ListOfGroups} />
+      <Stack.Screen
+        name="SingleGroup"
+        children={() => <SingleGroup enrollHandler={enrollHandler} />}
+        screenOptions={{ presentation: "modal" }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const ListOfGroups = ({ navigation }) => {
+  return (
     <View>
       <Text>ListOfGroups</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("SingleGroup")}>
+        <Text>View</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const SingleGroup = ({ enrollHandler }) => {
+  return (
+    <View>
+      <Text>SingleGroup</Text>
       <TouchableOpacity onPress={enrollHandler}>
         <Text>Enroll</Text>
       </TouchableOpacity>
